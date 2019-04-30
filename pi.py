161812135f9e991deca551f0295c67b2add21f8e
BeritP <BeritPorsche@googlemail.com>
2019-04-30 22:05:15 +0200

    fastfertig

--- pi.py
+++ pi.py
@@ -1,1 +1,12 @@
+import turtle as tu
+lines = 100
 
+with open("1_million_digits_of_pi.txt.txt", "r") as f: 
+    pi = f.read()
+
+for n in range(lines):
+    zahl = int(pi[n])
+    rotation = zahl * 36
+    tu.setheading(rotation)
+    tu.forward(3)
+turtle.done()
